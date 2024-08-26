46864b41fe9454ef9ac6faae21df43304cae3383
codingkunst <ryoo6931@gmail.com>
2024-08-26 11:38:16 +0900

    [Feat] settings apiUrl, apiKey

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Weather from './components/Weather';
+import WeatherButton from './components/WeatherButton';
 
 function App() {
+  const apiUrl = import.meta.env.VITE_SERVER_URL;
   const apiKey = import.meta.env.VITE_API_KEY;
+
+  const [weather, setWeather] = useState(null);
 
   // 현재 위치
   const getCurrentLocation = () => {
@@ -16,10 +22,11 @@ function App() {
 
   // 현재 날씨
   const getCurrentWeather = async (latitude, longitude) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let url = `${apiUrl}lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log('data: ', data);
+    setWeather(data);
   }
 
   useEffect(() => {
@@ -28,6 +35,10 @@ function App() {
 
   return (
     <div>
+      <div className='container'>
+        <Weather weather={weather} />
+        <WeatherButton />
+      </div>
     </div>
   )
 }
