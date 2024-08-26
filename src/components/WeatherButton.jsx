0835e0cd7d867bba79089f8d2d6f04e3aa3f5530
codingkunst <ryoo6931@gmail.com>
2024-08-26 11:35:16 +0900

    [Feat] UI 구현

---
+++ src/components/WeatherButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const WeatherButton = () => {
+    return (
+        <div>
+            <Button variant="outline-light">현재 위치</Button>
+            <Button variant="outline-light">현재 위치</Button>
+            <Button variant="outline-light">현재 위치</Button>
+            <Button variant="outline-light">현재 위치</Button>
+        </div>
+    )
+}
+
+export default WeatherButton
