f614b1f9168b790dfa342c17cbc2188e7b752ab0
codingkunst <ryoo6931@gmail.com>
2024-08-27 16:42:43 +0900

    [Feat] 도시 버튼 조회

--- src/components/WeatherButton.jsx
+++ src/components/WeatherButton.jsx
@@ -2,16 +2,11 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 const WeatherButton = (props) => {
-
-  const onWeahterHandler = () => {
-    console.log('현재 위치 날씨');
-  }
-
   return (
     <div>
       <Button variant="outline-light">현재 위치</Button>
-      {props.city.map((item, index) => (
-        <Button variant="outline-light" key={index} onClick={onWeahterHandler}>{item}</Button>
+      {props.citys.map((item, index) => (
+        <Button variant="outline-light" key={index} onClick={() => { props.setCity(item) }}>{item}</Button>
       ))}
     </div>
   )
